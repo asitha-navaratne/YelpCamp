85acb55448e83a2ef56716683e325509f7d3282e
Asitha Navaratne <asithan476@gmail.com>
2022-02-19 12:05:32 +0530

    Added random image and description to campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, earum voluptas! Sunt perspiciatis consectetur veritatis, sed non enim doloremque! Saepe velit eius, nam libero nostrum rerum adipisci? Ipsum, veniam ab!",
+      price,
     });
     await camp.save();
   }
