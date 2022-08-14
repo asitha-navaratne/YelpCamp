85beebeb4fd31f092b58ef7803dc845a3e06b4d2
Asitha Navaratne <asithan476@gmail.com>
2022-08-15 01:01:35 +0530

    Added mongo-sanitize

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const passport = require("passport");
 const passportLocal = require("passport-local");
 const User = require("./models/user");
 
+const mongoSanitize = require("express-mongo-sanitize");
+
 const ExpressError = require("./utils/ExpressError");
 
 const userRoutes = require("./routes/users");
@@ -39,6 +41,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
   secret: "thisshouldbeabettersecret!",
