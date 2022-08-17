8f56b415c35dbd0303631174fe78d51f85691863
Asitha Navaratne <asithan476@gmail.com>
2022-08-17 23:07:41 +0530

    Added certain variables from the env file

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-// const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
@@ -94,20 +93,22 @@ app.use(
   })
 );
 
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+
 const store = new MongoDBStore({
   url: dbUrl,
-  secret: "thisshouldbeabettersecret!",
+  secret,
   touchAfter: 24 * 60 * 60,
 });
 
 store.on("error", function (e) {
-  console.log("SESSION STORE ERROR!");
+  console.log("SESSION STORE ERROR!", e);
 });
 
 const sessionConfig = {
   store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
